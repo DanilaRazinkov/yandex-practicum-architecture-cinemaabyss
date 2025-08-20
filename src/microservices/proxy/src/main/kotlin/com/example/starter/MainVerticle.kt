3c17cfbaca3465d63f695243789f69208ff42654
danilarazinkov <danilarazinkov@redcollar.ru>
2025-08-20 22:57:26 +0300

    test work added

--- src/microservices/proxy/src/main/kotlin/com/example/starter/MainVerticle.kt
+++ src/microservices/proxy/src/main/kotlin/com/example/starter/MainVerticle.kt
@@ -4,11 +4,13 @@ import io.vertx.config.ConfigRetriever
 import io.vertx.config.ConfigRetrieverOptions
 import io.vertx.config.ConfigStoreOptions
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.proxy.handler.ProxyHandler
 import io.vertx.httpproxy.HttpProxy
 import io.vertx.kotlin.core.json.get
 import java.net.URI
+import kotlin.random.Random
 
 class MainVerticle : AbstractVerticle() {
 
@@ -27,33 +29,56 @@ class MainVerticle : AbstractVerticle() {
             val monolithUri = URI.create(config.get<String>("MONOLITH_URL"))
             val monolithProxy = HttpProxy.reverseProxy(proxyClient)
             monolithProxy.origin(monolithUri.port, monolithUri.host)
+            val monolithHandler = ProxyHandler.create(monolithProxy)
 
             val moviesUri = URI.create(config.get<String>("MOVIES_SERVICE_URL"))
             val moviesProxy = HttpProxy.reverseProxy(proxyClient)
             moviesProxy.origin(moviesUri.port, moviesUri.host)
+            val movieHandler = ProxyHandler.create(moviesProxy)
 
             val eventsUri = URI.create(config.get<String>("EVENTS_SERVICE_URL"))
             val eventsProxy = HttpProxy.reverseProxy(proxyClient)
             eventsProxy.origin(eventsUri.port, eventsUri.host)
+            val eventHandler = ProxyHandler.create(eventsProxy)
 
             val proxyRouter = Router.router(vertx)
 
             proxyRouter
-                .route("/health")
-                .handler(ProxyHandler.create(monolithProxy))
+                .route("/health").handler { ctx ->
+                    val healthResponse = JsonObject()
+                        .put("status", true)
+
+                    ctx.response()
+                        .setStatusCode(200)
+                        .putHeader("Content-Type", "application/json")
+                        .end(healthResponse.encode())
+                }
 
             proxyRouter
                 .route("/api/users*")
-                .handler(ProxyHandler.create(monolithProxy))
+                .handler(monolithHandler)
 
             proxyRouter
                 .route("/api/payments*")
-                .handler(ProxyHandler.create(monolithProxy))
+                .handler(monolithHandler)
 
+            proxyRouter
+                .route("/api/events*")
+                .handler(eventHandler)
+
+            val migrationPercent = config.get<Int>("MOVIES_MIGRATION_PERCENT")
 
             proxyRouter
                 .route("/api/movies*")
-                .handler(ProxyHandler.create(moviesProxy))
+                .handler { ctx ->
+                    val random = Random.nextInt(100)
+
+                    if (random < migrationPercent) {
+                        movieHandler.handle(ctx)
+                    } else {
+                        monolithHandler.handle(ctx)
+                    }
+                }
 
             val proxyServer = vertx.createHttpServer()
             proxyServer.requestHandler(proxyRouter)
